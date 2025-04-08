201f79952669e6f1d786c991ab94c6181a184524
ImNotDelta <mina.sagvolden.thai@gmail.com>
2025-04-09 01:47:32 +0200

    Add files via upload

--- GameRecord/app.mjs
+++ GameRecord/app.mjs
@@ -3,14 +3,17 @@ import Game from "./modules/game.mjs";
 
 const FILE_INPUT_ELEMENT = document.querySelector("input#fileUploader");
 const INDEX_DISPLAY_ELEMENT = document.querySelector("span#indexPosition");
-//const FILE_INDEXER_ELEMENT = document.querySelector("input[type=\"range\"]#inputSource");
 const OUTPUT_ELEMENT = document.querySelector("div#display");
-//const SHOW_ALL_BUTTON = document.querySelector("button#showAllBtn");
-//const ADD_NEW_GAME_BUTTON = document.querySelector("button#newGameFormAddButton");
-//let newGameFormElements = document.querySelector("#newGameFormTable").querySelectorAll("input, select, span");
+const SORT_ELEMENT = document.querySelector("select#sortOptions");
 let games = [];
 
 const STORAGE_NAME = "GameStorage";
+
+
+if(localStorage[STORAGE_NAME]) {
+    processFileContents(localStorage[STORAGE_NAME]);
+}
+
 
     
 function getData() {
@@ -56,13 +59,87 @@ FILE_INPUT_ELEMENT.addEventListener("change", function() {
 
 function processFileContents(fileData) {
     let jsonObjectOfContentsFromExampleDotJson = JSON.parse(fileData);
-    //FILE_INDEXER_ELEMENT.max = jsonObjectOfContentsFromExampleDotJson .length-1;
-    //FILE_INDEXER_ELEMENT.value = 0;
     saveData(jsonObjectOfContentsFromExampleDotJson );
-    games = jsonObjectOfContentsFromExampleDotJson ;
+
+    games = [];
+
+    for( let i = 0; i < jsonObjectOfContentsFromExampleDotJson.length; i++){
+        let col = jsonObjectOfContentsFromExampleDotJson[i];
+        games.push(new Game(col.title, col.designer, col.artist, col.publisher, col.year, col.players, col.time, col.difficulty, col.url, col.playCount, col.personalRating))
+    }
     console.log(games);
 
-   
+    saveData(games);
+
+    display();
+}
+
+function createEntry(index, propertyName, propertyValue) {
+    let brandElement = document.createElement("label");
+    brandElement.textContent = propertyName;
+    let containerElement = document.createElement("div");
+    containerElement.appendChild(brandElement);
+
+
+    let inputTextElement = document.createElement("input");
+        inputTextElement.setAttribute("index", index);
+        inputTextElement.setAttribute("propertyName", propertyName);
+        inputTextElement.value = propertyValue;
+        inputTextElement.addEventListener("change", function(){
+            games[this.getAttribute("index")][this.getAttribute("propertyName")] = this.value;
+            saveData(games);
+        });
+
+     if(propertyName==="personalRating") {
+        let inputRangeSlider = document.createElement("input");
+        inputRangeSlider.type = "range";
+        inputRangeSlider.setAttribute("index", index);
+        inputRangeSlider.setAttribute("propertyName", propertyName);
+        inputRangeSlider.value = propertyValue;
+        inputRangeSlider.addEventListener("change", function() {
+            games[this.getAttribute("index")][this.getAttribute("propertyName")] = this.value;
+            saveData(games);
+        });
+        inputRangeSlider.min = 0;
+        inputRangeSlider.max = 10;
+        containerElement.appendChild(inputRangeSlider);
+     } else if(propertyName==="playCount") {
+        let playCountButton = document.createElement("button");
+        playCountButton.textContent = "∆";
+        playCountButton.setAttribute("index", index);
+        playCountButton.setAttribute("propertyName", propertyName);
+        playCountButton.addEventListener("click", function() {
+            games[this.getAttribute("index")][this.getAttribute("propertyName")] ++;
+            this.parentElement.querySelector("input[index][propertyName]").value = games[this.getAttribute("index")][this.getAttribute("propertyName")];
+        });
+        containerElement.appendChild(inputTextElement);
+        containerElement.appendChild(playCountButton);
+     } else {
+        containerElement.appendChild(inputTextElement);
+     }
+
+     return containerElement;
+}
+
+document.querySelector("button#add").addEventListener("click", function() {
+    let title = document.getElementById("newTitle").value;
+    let designer = document.getElementById("newDesigner").value;
+    let artist = document.getElementById("newArtist").value;
+    let publisher = document.getElementById("newPublisher").value;
+    let year = document.getElementById("newYear").value;
+    let player = document.getElementById("newPlayer").value;
+    let time = document.getElementById("newTime").value;
+    let difficulty = document.getElementById("newDifficulty").value;
+    let url = document.getElementById("newURL").value;
+    let playCount = document.getElementById("newPlayCount").value;
+    let rating = document.getElementById("newRating").value;
+    games.push(new Game(title, designer, artist, publisher, year, player, time, difficulty, url, playCount, rating));
+    saveData(games);
+    display();
+});
+
+function display() {
+    OUTPUT_ELEMENT.innerHTML = "";
     for(let i = 0; i < games.length; i++) {
         let containerElement = document.createElement("div");
         containerElement.className = "game-entry";
@@ -72,30 +149,18 @@ function processFileContents(fileData) {
         }
         OUTPUT_ELEMENT.appendChild(containerElement);
     }
-    //updateDataFields(0);
-    //displayInfo();
 }
 
-function createEntry(index, propertyName, propertyValue) {
-    let brandElement = document.createElement("label");
-    brandElement.textContent = propertyName;
-    let containerElement = document.createElement("div");
-    containerElement.appendChild(brandElement);
+SORT_ELEMENT.addEventListener("change", function(){
+    sortBy(games, this.value);
+    display();
+})
 
-     if(propertyName==="personalRating") {
-        let inputRangeSlider = document.createElement("input");
-        inputRangeSlider.type = "range";
-        inputRangeSlider.setAttribute("index", index);
-        inputRangeSlider.setAttribute("propertyName", propertyName);
-        inputRangeSlider.value = propertyValue;
-        containerElement.appendChild(inputRangeSlider);
-     } else {
-        let inputTextElement = document.createElement("input");
-        inputTextElement.setAttribute("index", index);
-        inputTextElement.setAttribute("propertyName", propertyName);
-        inputTextElement.value = propertyValue;
-        containerElement.appendChild(inputTextElement);
-     }
-
-     return containerElement;
+function sortBy(array, propertyName) {
+    array.sort(function(a, b) {
+        if(a[propertyName]<b[propertyName])
+            return -1;
+        else
+            return 1;
+    });
 }
